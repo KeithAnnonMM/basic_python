90c7c37cfe118e87f519b464e633c3e21b3c5f96
KeithAnnonMM <keithmuwanguzi@gmail.com>
2022-12-17 11:19:48 +0300

    new changes

--- family.py
+++ family.py
@@ -1,6 +1,12 @@
 class Worker:
-    def __init__(self,name):
+    company = 'LBC'
+    
+    def __init__(self,name,gender):
         self.name = name
+        self.gender = gender
+
+    def getName(self):
+        return self.name
     
     def get_worker_name(self):
         print('Worker name:',self.name)
@@ -8,40 +14,57 @@ class Worker:
     def getWorker_sector(self):
         print('No sector yet')
 
+    def getPrefix(self):
+        if self.gender.lower() == 'm'or self.gender.lower() == 'male':
+            return 'He is'
+        elif self.gender.lower() == 'f'or self.gender.lower() == 'female':
+            return 'She is'
+        else:
+            return 'They are'
+    
+
 class Manager(Worker):
 
-    def __init__(self,name,sector):
-        super().__init__(name)
+    def __init__(self,name,sector,gender):
+        super().__init__(name,gender)
         self.sector = sector
+        self.prefix = Worker.getPrefix(self)
 
     def getWorker_sector(self):
-        print(self.name,'is the manager of the',self.sector,'department')
+        print(self.prefix,'the manager of the',self.sector,'department')
 
 class Hr(Manager):
-    def __init__(self,name,sector):
-        super().__init__(name,sector)
+    def __init__(self,name,sector,gender):
+        super().__init__(name,sector,gender)
+        self.prefix = Worker.getPrefix(self)
 
-    def __init__(self,name):
+    def __init__(self,name,gender):
         self.name = name
         self.sector = 'HR'
+        self.gender = gender
+        self.prefix = Worker.getPrefix(self)
 
 def getWorker(value):
-    value.getWorker_sector()
     value.get_worker_name()
+    print(value.getName(),'works for {}'.format(value.__class__.company))
+    value.getWorker_sector()
 
 class Cleaner(Worker):
-    def __init__(self,name,sector):
-        super().__init__(name)
+    
+    def __init__(self,name,sector,gender):
+        super().__init__(name,gender)
         self.sector = sector
+        self.prefix = Worker.getPrefix(self)
+
 
     def getWorker_sector(self):
-        print(self.name,'is part of the',self.sector,'department')
+        print(self.prefix,'part of the',self.sector,'department')
         
 
 
-human_resource = Hr('Alex Rukundo')
-cleaner = Cleaner('Diana Nakkidde','Bathroom Cleaning')
-ceo = Manager('Mukanu Dennis','Technology')
+human_resource = Hr('Alex Rukundo','M')
+cleaner = Cleaner('Diana Nakkidde','Bathroom Cleaning','F')
+ceo = Manager('Mukanu Dennis','Technology','Mal')
 
 workers = [human_resource,cleaner,ceo]
 for worker in workers:
